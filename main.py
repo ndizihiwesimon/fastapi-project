8a8a98fbba979f194ee2da859f75b3f557e3ad86
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2024-01-06 18:27:00 +0200

    models file

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+todos = []
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+# Get a single todo
+
+# Create a new todo
+
+# Update a todo
+
+# Delete a todo
